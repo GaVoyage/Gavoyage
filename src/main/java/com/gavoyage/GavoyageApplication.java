7435cd5e6b36c04f8dd9f8698dadfd081b595ae3
seo-jio <lio8625@naver.com>
2023-05-02 15:25:49 +0900

    init

---
+++ src/main/java/com/gavoyage/GavoyageApplication.java
@@ -0,0 +1,13 @@
+package com.gavoyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GavoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GavoyageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gavoyage/GavoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gavoyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GavoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
