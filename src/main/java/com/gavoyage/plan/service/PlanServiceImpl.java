8c0462e8d575701110da21a65fa8bac445d755cd
seo-jio <lio8625@naver.com>
2023-05-10 17:41:18 +0900

    Feat: 여행 계획에 해당하는 여행지 정보 조회

--- src/main/java/com/gavoyage/plan/service/PlanServiceImpl.java
+++ src/main/java/com/gavoyage/plan/service/PlanServiceImpl.java
@@ -1,7 +1,11 @@
 package com.gavoyage.plan.service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Service;
 
@@ -10,44 +14,68 @@ import com.gavoyage.plan.domain.DailyPlan;
 import com.gavoyage.plan.domain.Plan;
 import com.gavoyage.plan.dto.request.DailyPlanCreateDto;
 import com.gavoyage.plan.dto.request.PlanCreateReq;
+import com.gavoyage.region.domain.AttractionInfo;
+import com.gavoyage.region.service.RegionServiceImpl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class PlanServiceImpl implements PlanService{
 
+	private final RegionServiceImpl regionService;
 	private final PlanMapper planMapper;
 
 	@Override
-	public void createPlan(PlanCreateReq planCreateReq) {
+	public void createPlan(PlanCreateReq planCreateReq) throws Exception{
 		
 		planMapper.createPlan(planCreateReq);
 		
 		LocalDate curDate = planCreateReq.getStartDate();
-		Long planIdx = planCreateReq.getPlanIdx();
 		
+		Long planIdx = planCreateReq.getPlanIdx();
 		for(List<Integer> content_ids : planCreateReq.getDailyPlans()) {
+			
+			log.debug("curDate : " + curDate);
 			
 			for(Integer content_id : content_ids) {
 				planMapper.createDailyPlan(new DailyPlanCreateDto(planIdx, content_id, curDate));
 			}
 			
-			curDate.plusDays(1); // 날짜 증가
+			curDate = curDate.plusDays(1); // 날짜 증가
 		}
 	}
 
 	@Override
-	public List<Plan> findPlans(Long userIdx) {
+	public List<Plan> findPlans(Long userIdx) throws Exception{
 		return planMapper.getPlans(userIdx);
 	}
 
 	@Override
-	public List<DailyPlan> findDailyPlans(Long planIdx) {
+	public List<DailyPlan> findDailyPlans(Long planIdx) throws Exception{
 		return planMapper.getDailyPlans(planIdx);
 	}
 	
-	
-	
+	@Override
+	public Map<LocalDate, List<AttractionInfo>> findAllAttractionInfos(Long planIdx) throws Exception{
+		Map<LocalDate, List<AttractionInfo>> attractionInfos = new ConcurrentHashMap<>();
+		
+		for(DailyPlan dplan : findDailyPlans(planIdx)) {
+			
+			LocalDate dailyDate = dplan.getDailyDate();
+			AttractionInfo info = regionService.getAttractionInfosByContentId(dplan.getContent_id());
+			
+			if(!attractionInfos.containsKey(dailyDate)) {
+				attractionInfos.put(dailyDate, new ArrayList<>());
+			}
+			
+			attractionInfos.get(dailyDate).add(info);
+		
+		}
+		
+		return attractionInfos; 
+	}
 
 }
