eb87076d02835486f04b9f110f9470ac01ecd784
seo-jio <lio8625@naver.com>
2023-05-22 11:06:59 +0900

    Fix : aws s3 url 변경

--- src/main/java/com/gavoyage/config/aws/controller/AwsController.java
+++ src/main/java/com/gavoyage/config/aws/controller/AwsController.java
@@ -4,6 +4,7 @@ import java.net.URL;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/aws/s3")
 public class AwsController {
 	
 	private final AwsService awsService;
