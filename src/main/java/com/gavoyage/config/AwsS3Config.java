94bf7dca09a17849670f81e01a99460e012f212c
seo-jio <lio8625@naver.com>
2023-06-03 22:10:06 +0900

    Fix : git ignore 설정 추가

--- src/main/java/com/gavoyage/config/AwsS3Config.java
+++ src/main/java/com/gavoyage/config/AwsS3Config.java
@@ -3,6 +3,7 @@ package com.gavoyage.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
